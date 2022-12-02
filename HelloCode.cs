1f255973fbd2cf1843ea80c07be738542079165d
Jidnyasa-raut <62657093+Jidnyasa-raut@users.noreply.github.com>
2022-12-02 16:23:38 +0530

    github desktop code

--- HelloCode.cs
+++ HelloCode.cs
@@ -8,7 +8,7 @@ namespace CSharpSolutions
          static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hi World");
 
         }
     }
